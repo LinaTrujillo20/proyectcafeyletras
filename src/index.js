faaf1153358a1f9cd00559b0b4ff31e123cf405f
LinaTrujillo20 <linamariat298@gmail.com>
2022-06-09 20:35:28 -0500

    routes update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from "../src/components/login/Login";
 import Usuario from "../src/pages/Usuario";
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from "../src/pages/Home";
+import { BrowserRouter, Routes,Route, Navigate } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
+        <Route path='/proyectcafeyletras' element={<Home/>}></Route>
+        <Route path='/home' element={<Navigate replace to={"/proyectcafeyletras"}/>}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/usuario' element={<Usuario/>}></Route>
     </Routes>
